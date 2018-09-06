8a4f4083bdc5587e75d3781be8a7ba0d7614901f
JON <jonn.thinkrtc@gmail.com>
2018-09-06 12:03:09 -0400

    implemented PetProvider class to act as an abtract layer to fetch PetDb data.

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,17 +1,30 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class PetContract {
 
-    public static final class PetEntry implements BaseColumns {
-        public final static String TABLE_NAME = "pets";
+    private PetContract(){}
 
-        public final static String _ID = BaseColumns._ID;
-        public final static String COLUMN_PET_NAME ="name";
-        public final static String COLUMN_PET_BREED = "breed";
-        public final static String COLUMN_PET_GENDER = "gender";
-        public final static String COLUMN_PET_WEIGHT = "weight";
+
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_COTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
+
+    public static final class PetEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_COTENT_URI, PATH_PETS);
+        public static final String TABLE_NAME = "pets";
+
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PET_NAME ="name";
+        public static final String COLUMN_PET_BREED = "breed";
+        public static final String COLUMN_PET_GENDER = "gender";
+        public static final String COLUMN_PET_WEIGHT = "weight";
+
 
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
